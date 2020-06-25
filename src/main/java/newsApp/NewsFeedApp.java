0d39f7bf60d874fe828936c39f8d3b0e511ea1bf
Afgan Mustafayev <efqan.mustafayevstd99@gmail.com>
2020-06-25 13:17:23 +0400

    - program structure added

---
+++ src/main/java/newsApp/NewsFeedApp.java
@@ -0,0 +1,11 @@
+package newsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFeedApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NewsFeedApp.class);
+    }
+}
