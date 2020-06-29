2d5f4255352b668001d30b527c4f96708e532e92
Afgan Mustafayev <efqan.mustafayevstd99@gmail.com>
2020-06-29 23:46:56 +0400

    - minorChanges

--- src/main/java/newsApp/models/newsModel/News.java
+++ src/main/java/newsApp/models/newsModel/News.java
@@ -4,12 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class News {
+    @Id
+    private UUID id;
+
     private String title;
     private String news;
     private String source;
