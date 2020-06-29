41c63f42ba9cec9ebe655dffffb652cc2479a618
Afgan Mustafayev <efqan.mustafayevstd99@gmail.com>
2020-06-29 23:42:38 +0400

    - minorChanges

--- src/main/java/newsApp/controllers/userController/LoginController.java
+++ src/main/java/newsApp/controllers/userController/LoginController.java
@@ -1,4 +1,4 @@
-package newsApp.controller;
+package newsApp.controllers.userController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-
 public class LoginController {
 
     /**
