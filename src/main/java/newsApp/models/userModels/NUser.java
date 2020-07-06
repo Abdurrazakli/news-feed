f5481b372d5ff3e648725bc4cdd148347bc9e5da
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-07-06 19:55:06 +0400

    User model

--- src/main/java/newsApp/models/userModels/NUser.java
+++ src/main/java/newsApp/models/userModels/NUser.java
@@ -1,16 +1,15 @@
 package newsApp.models.userModels;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class NUser {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -19,6 +18,7 @@ public class NUser {
     private String fullname;
 
     @NotNull @NotEmpty
+    @Column(unique = true)
     private String email;
 
     @NotNull @NotEmpty
