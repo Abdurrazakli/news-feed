592477f45b0f7c78a2112446bada9890e40ecf28
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-07-06 19:54:52 +0400

    User Repo created

--- src/main/java/newsApp/repo/userRepo/NUserRepository.java
+++ src/main/java/newsApp/repo/userRepo/NUserRepository.java
@@ -1,7 +1,11 @@
 package newsApp.repo.userRepo;
 
 import newsApp.models.userModels.NUser;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface NUserRepository extends JpaRepository<NUser,Long> {
+    Optional<NUser> findByEmail(String email);
 }
