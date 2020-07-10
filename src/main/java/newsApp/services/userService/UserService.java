503e5f0e7af7b1aeed13f7c2b956fd800cdafc91
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-07-10 12:05:11 +0400

    merge bug fixed

--- src/main/java/newsApp/services/userService/UserService.java
+++ src/main/java/newsApp/services/userService/UserService.java
@@ -1,16 +1,25 @@
 package newsApp.services.userService;
 
+import lombok.extern.log4j.Log4j2;
 import newsApp.exceptions.userException.UserNotFoundException;
+import newsApp.models.newsModel.News;
 import newsApp.models.userModels.NUser;
+import newsApp.repo.newsRepo.NewsRepo;
 import newsApp.repo.userRepo.NUserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
+@Log4j2
 @Service
 public class UserService {
     private final NUserRepository userRepository;
+    private final NewsRepo newsRepo;
+    private final int PAGE_SIZE = 10;
+    private final String PARAM_PUBLISHED_DATE = "published_date";
 
-    public UserService(NUserRepository userRepository) {
+    public UserService(NUserRepository userRepository, NewsRepo newsRepo) {
         this.userRepository = userRepository;
+        this.newsRepo = newsRepo;
     }
 
     public NUser findByEmail(String email){
@@ -22,5 +31,20 @@ public class UserService {
         nUser.setPassword(newPassword);
         userRepository.save(nUser); // Fixme : Not update
 
+    }
+
+    public Page<News> loadLatestNewsPages(int page) {
+        Page<News> pages = Page.empty();
+        try {
+            //log.error(newsRepo.findAll().size());
+            //  pages = newsRepo.findAll(PageRequest.of(page, PAGE_SIZE, Sort.by(PARAM_PUBLISHED_DATE).descending()));
+            pages = newsRepo.findAll(PageRequest.of(page,PAGE_SIZE));
+
+            log.info(pages.getContent());
+        }
+        catch (Exception ex){
+            log.error("Page not found");
+        }
+        return pages;
     }
 }
